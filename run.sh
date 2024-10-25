72db0c170541f1376509323e4cbbfee6ec79158b
interactjoy <r@interactjoy.com>
2024-10-25 15:48:22 +1000

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 # Restart Miniconda
-cd /notebooks/miniconda3
-source /notebooks/miniconda3/etc/profile.d/conda.sh
+cd /notebooks/Miniconda
+source /notebooks/Miniconda/etc/profile.d/conda.sh
 echo "Conda version:"
 conda --version
 
 # Navigate to ToonCrafter and activate environment
-cd /notebooks/ToonCrafter
-conda activate tooncrafter
-echo "Activated environment: tooncrafter"
+cd /notebooks/animate
+conda activate animate
+echo "Activated environment: animate"
 
 # Run the Gradio app
-echo "Running gradio_app.py..."
+echo "Welcome back. Running Animation Software..."
 python gradio_app.py
